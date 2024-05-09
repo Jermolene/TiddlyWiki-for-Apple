dbabbc96ea673e8d303b8b285d71334704c0d473
Jeremy Ruston <jeremy@jermolene.com>
2024-05-09 16:01:44 +0100

    Initial Commit

---
+++ TiddlyWiki/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TiddlyWiki
+//
+//  Created by Jeremy Ruston on 09/05/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TiddlyWiki/TiddlyWikiApp.swift
@@ -0,0 +1,17 @@
+//
+//  TiddlyWikiApp.swift
+//  TiddlyWiki
+//
+//  Created by Jeremy Ruston on 09/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct TiddlyWikiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
