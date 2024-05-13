8b03beb3761e21716c566103224ebd9b39316149
Jeremy Ruston <jeremy@jermolene.com>
2024-05-13 09:55:09 +0100

    Use Swifter HTTP server

--- TiddlyWiki/TiddlyWikiApp.swift
+++ TiddlyWiki/TiddlyWikiApp.swift
@@ -6,17 +6,24 @@
 //
 
 import SwiftUI
+import Swifter
 
 @main
 struct TiddlyWikiApp: App {
-    var httpServer = HTTPServer(port: 8080)
+    let server = HttpServer()
+
     init() {
+        server["/"] = { request in
+            return .ok(.htmlBody("This is a web page served from \(request.path)"))
+        }
         do {
-            try httpServer.start()
+            try server.start(8080)  // Specifying a port is usually a good practice
+            print("Server started on port 8080")
         } catch {
-            print("Failed to start the server: \(error)")
+            print("Failed to start server: \(error)")
         }
     }
+
     var body: some Scene {
         WindowGroup {
             ContentView()
