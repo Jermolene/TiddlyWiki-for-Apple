d506dde806a1b63ec5becfbad47800cb52dce7d1
Jeremy Ruston <jeremy@jermolene.com>
2024-06-05 15:21:43 +0100

    Tweak refresh button

--- TiddlyWiki/ContentView.swift
+++ TiddlyWiki/ContentView.swift
@@ -14,26 +14,22 @@ struct ContentView: View {
         VStack {
             HStack {
                 Text("Welcome to TiddlyWiki")
-                Image(systemName: "globe")
-                    .imageScale(.large)
-                    .foregroundStyle(.tint)
                 Button(action: {
                     // Toggle the reload trigger to refresh the WebView
                     DispatchQueue.main.async {
                         reloadTrigger.toggle()
                     }
                 }) {
-                    Text("Refresh")
+                    Label("Refresh", systemImage: "arrow.clockwise")
                         .padding()
                         .background(Color.blue)
                         .foregroundColor(.white)
                         .cornerRadius(8)
                 }
+                
             }
             WebView(url: URL(string: "http://localhost:8080")!, reloadTrigger: $reloadTrigger)
-
         }
-        .padding()
     }
 }
 
