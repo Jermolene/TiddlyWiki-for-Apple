8e269c397215e9a7ff9461ed5d3222b991f52359
Jeremy Ruston <jeremy@jermolene.com>
2024-06-08 15:27:39 +0100

    Extend the WebView to retry after "connection refused" errors

--- TiddlyWiki/WebView.swift
+++ TiddlyWiki/WebView.swift
@@ -4,7 +4,6 @@
 //
 //  Created by Jeremy Ruston on 09/05/2024.
 //
-
 import Foundation
 import SwiftUI
 import WebKit
@@ -17,6 +16,8 @@ struct WebView: UIViewRepresentable {
 
     class Coordinator: NSObject, WKNavigationDelegate {
         var parent: WebView
+        var retryCount: Int = 0
+        var maxRetries: Int = 5
 
         init(parent: WebView) {
             self.parent = parent
@@ -26,7 +27,22 @@ struct WebView: UIViewRepresentable {
             let request = URLRequest(url: parent.url)
             webView.load(request)
         }
-        
+
+        // Handle "connection refused" errors by retrying after 1 second
+        // This is to work around the fact that the webview is created before the server is fully up and running
+        func webView(_ webView: WKWebView, didFailProvisionalNavigation navigation: WKNavigation!, withError error: Error) {
+            if let error = error as? URLError, error.code == .cannotConnectToHost {
+                retryCount += 1
+                if retryCount <= maxRetries {
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
+                        self.reload(webView)
+                    }
+                } else {
+                    retryCount = 0
+                }
+            }
+        }
+
         func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
             if let url = navigationAction.request.url, navigationAction.navigationType == .linkActivated {
                 self.parent.safariURL = url
